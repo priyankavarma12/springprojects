5e392abe0ac2cead05b8eee66d30a8ba4fc2faaf
priyanka_varma <priyankavarma717@gmail.com>
2020-09-29 17:26:47 +0530

    discovery service add

---
+++ discoveryservice/src/test/java/com/springjpop/discovery/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpop.discovery.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
